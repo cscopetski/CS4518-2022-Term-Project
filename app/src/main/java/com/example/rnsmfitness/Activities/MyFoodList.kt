0184f1e816c53c3e5fd0cf93918ad61a9c84c9c6
charkw <cckneisslwilliam@wpi.edu>
2022-09-23 16:49:18 -0400

    start of folder organization and myfoodlist

--- app/src/main/java/com/example/rnsmfitness/Activities/MyFoodList.kt
+++ app/src/main/java/com/example/rnsmfitness/Activities/MyFoodList.kt
@@ -1,11 +1,12 @@
-package com.example.rnsmfitness
+package com.example.rnsmfitness.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.rnsmfitness.R
 
-class MainActivity : AppCompatActivity() {
+class MyFoodList : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_my_food_list)
     }
 }
