f48a8cbfc632f999c68e6d823ec8d301db68196b
FlaggedID <ezhang2@wpi.edu>
2022-10-05 15:26:08 -0400

    finished xml file

--- app/src/main/java/com/example/rnsmfitness/Activities/SignUp.kt
+++ app/src/main/java/com/example/rnsmfitness/Activities/SignUp.kt
@@ -2,6 +2,7 @@ package com.example.rnsmfitness.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.rnsmfitness.R
 
 class SignUp : AppCompatActivity() {
@@ -9,4 +10,12 @@ class SignUp : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
     }
+
+    fun radio_button_click(view: View) {}
+    fun WeightGoal1Click(view: View) {}
+    fun WeightGoal2Click(view: View) {}
+    fun WeightGoal3Click(view: View) {}
+    fun onActiveLevelButtonClick(view: View) {}
+    fun onSexClick(view: View) {}
+    fun WeightGoalClick(view: View) {}
 }
