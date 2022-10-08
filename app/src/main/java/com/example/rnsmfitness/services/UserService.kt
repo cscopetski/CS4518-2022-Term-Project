a440aa0c98b329e53b60f963bf10056d5441093d
cscopetski <cscopetski@gmail.com>
2022-10-08 01:47:49 -0400

    added signup route to userservice

--- app/src/main/java/com/example/rnsmfitness/services/UserService.kt
+++ app/src/main/java/com/example/rnsmfitness/services/UserService.kt
@@ -1,18 +1,21 @@
 package com.example.rnsmfitness.services
 
 import com.example.rnsmfitness.Entities.LoginCredentials
+import com.example.rnsmfitness.Entities.SignUpCredentials
 import com.example.rnsmfitness.Entities.User
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 private const val URL_PREFIX = "/api/users"
 
 interface UserService {
 
 
-
     @GET("$URL_PREFIX/get")
     fun getUser(): Call<User>
 
+    @POST("$URL_PREFIX/signup")
+    fun signupUser(@Body signUpCredentials: SignUpCredentials): Call<User>
 }
