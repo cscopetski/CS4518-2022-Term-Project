026406b00dc4d8fb733c0d8c65320e37982b06d4
cscopetski <cscopetski@gmail.com>
2022-10-12 13:45:11 -0400

    Update SplashActivity.kt

--- app/src/main/java/com/example/rnsmfitness/Activities/SplashActivity.kt
+++ app/src/main/java/com/example/rnsmfitness/Activities/SplashActivity.kt
@@ -3,7 +3,9 @@ package com.example.rnsmfitness.Activities
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.rnsmfitness.RetroFitClient
 
 class SplashActivity : AppCompatActivity() {
     /** Duration of wait  */
@@ -16,7 +18,14 @@ class SplashActivity : AppCompatActivity() {
 
         /* New Handler to start the Menu-Activity
          * and close this Splash-Screen after some seconds.*/Handler().postDelayed({ /* Create an Intent that will start the Menu-Activity. */
-            val mainIntent = Intent(this@SplashActivity, HomeActivity::class.java)
+            var mainIntent: Intent
+            if(RetroFitClient.checkCookie()){
+                mainIntent = Intent(this@SplashActivity, HomeActivity::class.java)
+
+            }else {
+                mainIntent = Intent(this@SplashActivity, LoginActivity::class.java)
+            }
+
             this@SplashActivity.startActivity(mainIntent)
             finish()
         }, SPLASH_DISPLAY_LENGTH.toLong())
